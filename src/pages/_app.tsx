9feac3db25511cc6a584e906656e0bc3ee75bf38
Fabio Arazaki <fabioarazaki@gmail.com>
2024-12-02 17:54:55 -0300

    chore: lint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,27 @@
-import { AppProps } from 'next/app';
-import Head from 'next/head';
-import Script from 'next/script';
-import { FC } from 'react';
-import { SessionProvider } from 'next-auth/react';
-import { ContextProvider } from '../contexts/ContextProvider';
-import NavBar from '../components/nav-element/NavBar';
-import { ContentContainer } from '../components/ContentContainer';
-import Notifications from '../components/Notification';
-import '../styles/profile.css'; // Adjust the path based on your project structure.
-require('@solana/wallet-adapter-react-ui/styles.css');
-require('../styles/globals.css');
+import { AppProps } from "next/app"
+import Head from "next/head"
+import Script from "next/script"
+import { FC } from "react"
+import { SessionProvider } from "next-auth/react"
+import { ContextProvider } from "../contexts/ContextProvider"
+import NavBar from "../components/nav-element/NavBar"
+import { ContentContainer } from "../components/ContentContainer"
+import Notifications from "../components/Notification"
+import "../styles/profile.css" // Adjust the path based on your project structure.
+require("@solana/wallet-adapter-react-ui/styles.css")
+require("../styles/globals.css")
 
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base"
 import {
   ConnectionProvider,
-  WalletProvider
-} from '@solana/wallet-adapter-react';
+  WalletProvider,
+} from "@solana/wallet-adapter-react"
 import {
   PhantomWalletAdapter,
-  SolflareWalletAdapter
-} from '@solana/wallet-adapter-wallets';
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets"
 
-const wallets = [
-  new PhantomWalletAdapter(),
-  new SolflareWalletAdapter()
-];
+const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()]
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
@@ -37,15 +34,15 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
         src="https://telegram.org/js/telegram-widget.js?22"
         strategy="afterInteractive"
         onLoad={() => {
-          console.log('Telegram widget script loaded');
+          console.log("Telegram widget script loaded")
         }}
       />
 
       <ContextProvider>
-        <div className="flex flex-col min-h-screen">
+        <div className="flex min-h-screen flex-col">
           <Notifications />
           <NavBar />
-          <main className="flex-1 flex justify-center">
+          <main className="flex flex-1 justify-center">
             <ContentContainer>
               <Component {...pageProps} />
             </ContentContainer>
@@ -53,7 +50,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
         </div>
       </ContextProvider>
     </SessionProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
