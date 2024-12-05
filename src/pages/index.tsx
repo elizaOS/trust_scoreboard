e883a9cfcf833fc2e6329b3e884860da7835afea
Fabio Arazaki <fabioarazaki@gmail.com>
2024-12-04 21:06:50 -0300

    feat: make navbar page scoped

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,9 @@
 import type { NextPage } from "next"
 import Head from "next/head"
 import { HomeView } from "../views/home/index"
+import NavBar from "@/components/nav-element/NavBar"
+import Link from "next/link"
+import { ContentContainer } from "@/components/ContentContainer"
 
 const Home: NextPage = () => {
   return (
@@ -10,7 +13,21 @@ const Home: NextPage = () => {
         <title>ai16z - Marc&apos;s Trust Leaderboard</title>
         <meta name="description" content="ai16z - Marc's Trust Leaderboard" />
       </Head>
-      <HomeView />
+      <NavBar>
+        <Link
+          href="https://discord.com/invite/ai16z"
+          className="inline-flex items-center rounded-full bg-white/20 px-3.5 py-2 transition-all duration-300 hover:bg-white/30"
+        >
+          <span className="text-base font-semibold text-white">
+            Join Discord
+          </span>
+        </Link>
+      </NavBar>
+      <main className="flex flex-1 justify-center">
+        <ContentContainer>
+          <HomeView />
+        </ContentContainer>
+      </main>
     </div>
   )
 }
